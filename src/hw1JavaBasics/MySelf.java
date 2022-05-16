ec01389c2e0628d60139e749d9eb8cc4cb45b8e0
ryhnchwdhry <104733459+ryhnchwdhry@users.noreply.github.com>
2022-05-16 17:49:47 -0400

    May 16 5:49pm

---
+++ src/hw1JavaBasics/MySelf.java
@@ -0,0 +1,28 @@
+package hw1JavaBasics;
+/* 
+ * Naming convention -
+ * 
+ * Project name starts with upper case.
+ * Camel case example for a project --> January2022CoreJava
+ * Alternative example of Camel case, for a project --> January_2022_core_java 
+ * 
+ * Package name starts with the lower case.
+ * Camel case example for a package --> lec01JavaBasics
+ * Alternative example of Camel case, for a package --> lec01_java_basics
+ * 
+ * Class name starts with the UperCase
+ * Class names are usually nouns or noun phrases.
+ * Camel case example for a class --> NamingConventions
+ * Alternative example of Camel case, for a class --> Naming_conventions
+ */
+
+public class MySelf{
+	
+	// Main method is Entry point for Java. 
+	
+	public static void main(String[] args) {
+		System.out.println("Rayhan Chowdhury");
+		System.out.println("42 Alice ave Buffalo NY");
+	}
+
+}
---
+++ src/hw2JavaBasics/MyCar.java
@@ -0,0 +1,22 @@
+package hw2JavaBasics;
+
+public class MyCar {
+	public static void main(String[] args) {
+		System.out.println("My name is  \"Rayhan Chowdhury\"");
+		System.out.println("I love to drive, My car info is below:-");
+		System.out.println(" 2018 Honda Civic Saports " + " Hill Start Assist ");
+		System.out.println("1.5 liter turbo engine" + "Eco Assist™ System ");
+
+		System.out.println("Black colour " + "CARB Emissions Rating2	");
+		System.out.println("Front wheel Drive" + "Remote Engine Start");
+
+		System.out.print(
+				" Push Button Start Honda Link Speed-Sensitive Volume Compensation (SVC) \n Apple CarPlay and Android Auto Illuminated Steering Wheel-Mounted Controls.7-inch Color LCD Screen Multi-Angle Rearview Camera. \n\t Automatic Climate Control System. ");
+
+		System.out.println("Rayhab");
+	}
+	
+	
+	
+
+}
